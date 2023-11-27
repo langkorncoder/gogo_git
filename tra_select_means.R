fb3d39f8575dbf1777728e84847fb43f144dd16b
langkorncoder <128065162+langkorncoder@users.noreply.github.com>
2023-11-27 04:34:29 +0100

    Add files via upload

---
+++ tra_select_means.R
@@ -0,0 +1,19 @@
+tra_select_means <- function(input, output_file = "means_bundesland/means_bundesland.csv", selects = NULL, date_format = c("dmy", "mdy", "ymd", "dBy", "mdBy", "ymdHMS"), to_csv = FALSE) {
+  
+
+  means_select <- tibble::as_tibble(input)
+
+if (!is.null(selects)) {
+  means_select <- means_select %>%
+    dplyr::select(all_of(selects)) %>%
+    dplyr::mutate(year = year(lubridate::parse_date_time(date_col, orders = date_format)),
+           month = month(lubridate::parse_date_time(date_col, orders = date_format)))
+
+  means_structure <- structured(means_select)
+
+if (to_csv == TRUE) {
+  readr::write_csv(means_select, file = output_file)
+   }
+  
+	df <- return(list(means_select = means_select, means_structure = means_structure))
+}
